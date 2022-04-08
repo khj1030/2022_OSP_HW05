af03ee15e2f7a52ce9457950628567fc6ec670a6
khj1030 <khj011030@gmail.com>
2022-04-09 04:15:50 +0900

    Print top n-dictionary words

--- word_freq.py
+++ word_freq.py
@@ -9,14 +9,19 @@ lines = file.readlines()
 word_dict = {}
 for line in lines:
 	for word in line.split():
-		word = re.sub(r"[^a-zA-Z0-9]", "", word).lower()
+		word = re.sub(r"[^a-zA-Z]", "", word).lower()
 		if word!="":
 			count = word_dict.get(word,0)
 			word_dict[word] = count + 1
 
+word_list = word_dict.keys()
 
-print(word_dict)
-
+rank = 0
+for word in word_list:
+	if rank == int(sys.argv[2]):
+		break
+	print(word, word_dict[word])
+	rank = rank + 1
 
 file.close()
 
