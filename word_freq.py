3395474bbc85aa68bb99131f382584f85c7278d1
khj1030 <khj011030@gmail.com>
2022-04-09 04:02:24 +0900

    Make dictionary in word_freq.py

--- word_freq.py
+++ word_freq.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python
 
 import sys
+import re
 
 file = open(sys.argv[1], 'r')
 lines = file.readlines()
 
+word_dict = {}
 for line in lines:
-	print(line)
+	for word in line.split():
+		word = re.sub(r"[^a-zA-Z0-9]", "", word).lower()
+		if word!="":
+			count = word_dict.get(word,0)
+			word_dict[word] = count + 1
+
+
+print(word_dict)
 
 
 file.close()
+
+
+
 
